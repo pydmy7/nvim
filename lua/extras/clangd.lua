d867539520ba8dd91c00f0aad5b74695dff0747a
wangyao <1822364545@qq.com>
2025-09-20 10:06:57 +0800

    plugins for extras

--- lua/extras/clangd.lua
+++ lua/extras/clangd.lua
@@ -1,3 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.ai.copilot" },
+  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
 }
